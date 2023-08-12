74e84352d424d17f6aefb28dfc60b2d04524ed07
Edwinchege <edwinchegee@gmail.com>
2023-08-11 17:16:23 -0700

    combinations of single-digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+    	if (num != 9)
+	{
+		putchar(num + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	else
+	{
+		putchar(num + '0')
+	}
+	num++;
+	}
+	return (0);
+}
