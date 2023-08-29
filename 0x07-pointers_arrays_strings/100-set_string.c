4729d57136389cec566ad900c378bb2614e0c2ce
Edwinchege <edwinchegee@gmail.com>
2023-08-28 18:04:59 -0700

    Advanced Task

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_string – sets value of a pointer to a char
+ * set_string - sets value of a pointer to a char
  * @s: pointer-to-pointer to a char
  * @to: pointer to a char
  */
