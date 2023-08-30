aa925cdef1c23c230d3bf6f1c457b1048b55e6f6
Edwinchege <edwinchegee@gmail.com>
2023-08-30 14:48:54 -0700

    Advanced Task

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks if string is a palindrome
+ * @s: string to check
+ * Return: 1 success, otherwise 0
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - calculates the length of the string
+ * @s: string to be used
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - checks if the string is a palindrome recursively
+ * @s: string to check
+ * @i: index to compare characters
+ * @len: length of string
+ * Return: 1 success, otherwise 0
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+}
